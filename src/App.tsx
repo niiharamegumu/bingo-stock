3ad1bb8113102258f95ee9e8a778ce3d4740ddb8
Niihara Megumu <nhrmgm1223@gmail.com>
2022-09-05 18:09:40 +0900

    add main

--- src/App.tsx
+++ src/App.tsx
@@ -1,103 +1,52 @@
-import { Container, Grid, Button, Flex, Input } from '@chakra-ui/react'
-import { useToast } from '@chakra-ui/react'
-import { ChangeEvent, useEffect, useState } from 'react'
-import { IoIosAddCircle } from 'react-icons/io'
+import { Flex, Grid, Text } from '@chakra-ui/react'
+import { doc, onSnapshot } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
 import { Card } from './components/card'
-import { useCardList } from './hooks/useCardList'
+import db from './libs/firebase'
 
-function App() {
-  const cardList = useCardList(1, 75)
-  const [val, setVal] = useState<number>(0)
+const App = () => {
   const [alreadyList, setAlreadyList] = useState<number[]>([])
-  const toast = useToast()
 
   useEffect(() => {
-    const lsAlreadyList = localStorage.getItem('alreadyList')
-    if (lsAlreadyList) {
-      setAlreadyList(JSON.parse(lsAlreadyList))
+    const init = async () => {
+      const alreadyListRef = doc(db, 'lists', 'alreadyList')
+      onSnapshot(alreadyListRef, (doc) => {
+        if (doc.exists()) {
+          const numbers = doc.data().numbers as number[]
+          setAlreadyList(numbers)
+        }
+      })
     }
+    init()
   }, [])
 
-  const onChangeVal = (e: ChangeEvent<HTMLInputElement>) => {
-    setVal(Number(e.target.value))
-  }
-  const resetList = () => {
-    setAlreadyList([])
-    localStorage.removeItem('alreadyList')
-  }
-  const addAlreadyList = () => {
-    const errors: string[] = []
-    if (!val) {
-      errors.push('空では追加できません')
-    }
-    if (alreadyList.includes(val)) {
-      errors.push('すでに出ている数字です')
-    }
-    if (!cardList.includes(val) && val) {
-      errors.push('対象範囲画の数字です')
-    }
-
-    if (errors.length === 0) {
-      setAlreadyList((p) => {
-        localStorage.setItem('alreadyList', JSON.stringify([...p, val]))
-        return [...p, val]
-      })
-    } else {
-      errors.map((title) => {
-        toast({
-          title,
-          status: 'error',
-          duration: 2000,
-          isClosable: true,
-          position: 'bottom-right',
-        })
-      })
-    }
-  }
-
   return (
-    <Container maxW="100vw" pt={4}>
-      <Grid
-        templateColumns={{
-          base: 'repeat(4, 1fr)',
-          md: 'repeat(6, 1fr)',
-          xl: 'repeat(8, 1fr)',
-        }}
-        columnGap={8}
-        mb={4}
-      >
-        {alreadyList.map((list) => (
-          <Card key={list}>{list}</Card>
-        ))}
-      </Grid>
-      <Flex justify="space-between" mb={8}>
-        <Flex justify="center" alignItems="center">
-          <Button
-            bg="sTNotice"
-            borderRadius={6}
-            color="gray.200"
-            px={4}
-            h="100%"
-            onClick={resetList}
-          >
-            リセット
-          </Button>
+    <>
+      {alreadyList.length > 0 ? (
+        <Grid
+          templateColumns="repeat(6, 1fr)"
+          maxW="90vw"
+          margin="auto"
+          rowGap={2}
+          py={4}
+        >
+          {alreadyList.map((list) => (
+            <Card key={list}>{list}</Card>
+          ))}
+        </Grid>
+      ) : (
+        <Flex
+          justify="center"
+          alignItems="center"
+          h="100vh"
+          textAlign="center"
+          flexDirection="column"
+          gap={4}
+        >
+          <Text>ビンゴ開始までもうしばらくお待ち下さい...</Text>
         </Flex>
-        <Flex gap={4}>
-          <Input
-            maxW="100px"
-            borderColor="sTMainColor"
-            type="number"
-            onChange={(e) => {
-              onChangeVal(e)
-            }}
-          />
-          <Button onClick={addAlreadyList}>
-            <IoIosAddCircle fontSize="40px" />
-          </Button>
-        </Flex>
-      </Flex>
-    </Container>
+      )}
+    </>
   )
 }
 
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -7,7 +7,5 @@ type Props = {
 
 export const Card: FC<Props> = (props) => {
   const { children } = props
-  return (
-    <Center fontSize={{ base: '3vw', sm: '5vw', md: '7vw' }}>{children}</Center>
-  )
+  return <Center fontSize="8vw">{children}</Center>
 }
