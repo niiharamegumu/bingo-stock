48506b08b5bcd4e8ec9aa0c1c1af46437993e75a
Niihara Megumu <nhrmgm1223@gmail.com>
2022-09-05 10:22:01 +0900

    add chakra ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import theme from './theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider resetCSS theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
 )
 
